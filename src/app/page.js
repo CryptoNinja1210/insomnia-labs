a4d5db201ce19d62bd3d169389bf36b08b113ff6
CryptoNinja1210 <jerome.pangilinan914@gmail.com>
2024-06-10 12:18:07 +0800

    get owned nfts

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,47 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client'
 
 import Image from "next/image";
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 import AddressContext from '@/context/AddressContext'
+import { Alchemy, Network } from "alchemy-sdk";
+
+const config = {
+  apiKey: "Pjyj1p5qvYMQaEXdUFH3p7beiWlzpKhc",
+  network: Network.ETH_MAINNET,
+};
+const alchemy = new Alchemy(config);
 
 export default function Home() {
   const addrInfo = useContext(AddressContext);
-    console.log('props', addrInfo)
+  // const { nfts, loading, error } = useNfts(addrInfo);
+  // console.log(nfts, loading, error)
+
   useEffect(() => {
-    console.log('props', addrInfo)
+    if (addrInfo) {
+      getNfts(addrInfo)
+    }
   }, [addrInfo]);
 
+  const getNfts = async (address) => {
+    const nfts = await alchemy.nft.getNftsForOwner(address);
+
+    const numNfts = nfts["totalCount"];
+    const nftList = nfts["ownedNfts"];
+
+    console.log(`Total NFTs owned by ${address}: ${numNfts} \n`);
+
+    let i = 1;
+
+    for (let nft of nftList) {
+      console.log(`${i}. ${nft.title}`);
+      i++;
+    } 
+  }
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+
     </main>
   );
 }
